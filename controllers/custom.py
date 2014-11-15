1124f58278ba4a5caa84037de8c0a819dbad22f6
shwethag <g.shwetha.g@gmail.com>
2014-11-16 01:15:56 +0530

    Added menu items

--- controllers/custom.py
+++ controllers/custom.py
@@ -4,8 +4,12 @@ def index(): return dict()
 
 @auth.requires_login() 
 def home():
-    return dict(message='hello from internal home')
+   return dict(message='hello from internal home')
 
 @auth.requires_login() 
 def happy():
-    return dict(message='hello from happy')    
+    return dict(message='hello from happy')
+
+@auth.requires_login()
+def profile():
+    redirect(URL('default','user',args='profile'))
